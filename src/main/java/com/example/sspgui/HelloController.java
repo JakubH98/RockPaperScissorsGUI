94f0c13407503004e15d45999973253495afb43a
JakubH98 <jakubsmail8@gmail.com>
2024-02-25 16:05:55 +0100

    refactored game code

--- src/main/java/com/example/sspgui/HelloController.java
+++ src/main/java/com/example/sspgui/HelloController.java
@@ -1,7 +1,6 @@
 package com.example.sspgui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import java.util.Random;
 
@@ -15,95 +14,49 @@ public class HelloController {
     @FXML
     private Label cpuWinText;
 
-    Random random = new Random();
-
+    private Random random = new Random();
     private String[] cpuChoice = {"Rock", "Paper", "Scissors"};
-    private int randomChoice;
-    private String playerChoice;
     private int cpuWins = 0;
     private int playerWins = 0;
 
-
-
     @FXML
     protected void scissorsButtonClicked() {
-        cpuText.setText("");
-        playerWinText.setText("Player wins: " + playerWins);
-        cpuWinText.setText("Cpu wins: " + cpuWins);
-
-        playerChoice = "Scissors";
-        randomChoice = random.nextInt(3);
-        cpuText.setText("Cpu chose: " + cpuChoice[randomChoice]);
-        if (cpuChoice[randomChoice].equalsIgnoreCase(playerChoice))
-        {
-            outcomeText.setStyle("-fx-text-fill: black;");
-            outcomeText.setText("It's a tie!");
-        }
-        else if (cpuChoice[randomChoice].equalsIgnoreCase("Rock"))
-        {
-            cpuWins++;
-            outcomeText.setStyle("-fx-text-fill: red;");
-            outcomeText.setText("You lose!");
-            cpuWinText.setText("Cpu wins: " + cpuWins);
-        }
-        else if (cpuChoice[randomChoice].equalsIgnoreCase("Paper"))
-        {
-            playerWins++;
-            outcomeText.setStyle("-fx-text-fill: green;");
-            outcomeText.setText("You win!");
-            playerWinText.setText("Player wins: " + playerWins);
-        }
+        playRound("Scissors");
     }
+
     @FXML
     protected void paperButtonClicked() {
+        playRound("Paper");
+    }
+
+    @FXML
+    protected void rockButtonClicked() {
+        playRound("Rock");
+    }
+
+    private void playRound(String playerChoice) {
         cpuText.setText("");
         playerWinText.setText("Player wins: " + playerWins);
         cpuWinText.setText("Cpu wins: " + cpuWins);
 
-        playerChoice = "Paper";
-        randomChoice = random.nextInt(3);
+        int randomChoice = random.nextInt(3);
         cpuText.setText("Cpu chose: " + cpuChoice[randomChoice]);
-        if (cpuChoice[randomChoice].equalsIgnoreCase(playerChoice))
-        {
+
+        if (cpuChoice[randomChoice].equalsIgnoreCase(playerChoice)) {
             outcomeText.setStyle("-fx-text-fill: black;");
             outcomeText.setText("It's a tie!");
-        }
-        else if (cpuChoice[randomChoice].equalsIgnoreCase("Scissors"))
-        {
+        } else if ((cpuChoice[randomChoice].equalsIgnoreCase("Rock") && playerChoice.equalsIgnoreCase("Scissors")) ||
+                (cpuChoice[randomChoice].equalsIgnoreCase("Paper") && playerChoice.equalsIgnoreCase("Rock")) ||
+                (cpuChoice[randomChoice].equalsIgnoreCase("Scissors") && playerChoice.equalsIgnoreCase("Paper"))) {
             cpuWins++;
             outcomeText.setStyle("-fx-text-fill: red;");
             outcomeText.setText("You lose!");
             cpuWinText.setText("Cpu wins: " + cpuWins);
-        }
-        else if (cpuChoice[randomChoice].equalsIgnoreCase("Rock"))
-        {
+        } else {
             playerWins++;
             outcomeText.setStyle("-fx-text-fill: green;");
             outcomeText.setText("You win!");
             playerWinText.setText("Player wins: " + playerWins);
-        }
-    }
-    @FXML
-    protected void rockButtonClicked() {
-        cpuText.setText("");
-        playerChoice = "Rock";
-        randomChoice = random.nextInt(3);
-        cpuText.setText("Cpu chose: " + cpuChoice[randomChoice]);
-
-        if (cpuChoice[randomChoice].equalsIgnoreCase(playerChoice))
-        {
-            outcomeText.setStyle("-fx-text-fill: black;");
-            outcomeText.setText("It's a tie!");
-        }
-        else if (cpuChoice[randomChoice].equalsIgnoreCase("Paper"))
-        {
-            outcomeText.setStyle("-fx-text-fill: red;");
-            outcomeText.setText("You lose!");
-        }
-        else if (cpuChoice[randomChoice].equalsIgnoreCase("Scissors"))
-        {
-            outcomeText.setStyle("-fx-text-fill: green;");
-            outcomeText.setText("You win!");
         }
     }
 }
