5ff757faef9fc6ef95b4ffe7617a44080e2c8f06
JakubH98 <jakubsmail8@gmail.com>
2024-03-28 09:56:13 +0100

    Added image&text on player choice

--- src/main/java/com/example/sspgui/HelloController.java
+++ src/main/java/com/example/sspgui/HelloController.java
@@ -9,7 +9,9 @@ import java.util.Random;
 
 public class HelloController {
     @FXML
-    private Label cpuText;
+    private Label cpuChoiceText;
+    @FXML
+    private Label playerChoiceText;
     @FXML
     private Label outcomeText;
     @FXML
@@ -18,36 +20,47 @@ public class HelloController {
     private Label cpuWinText;
     @FXML
     private ImageView cpuChoiceImage;
-
+    @FXML
+    private ImageView playerChoiceImage;
 
     private Random random = new Random();
-    private String[] cpuChoice = {"Rock", "Paper", "Scissors"};
+    private final String[] cpuChoice = {"Rock", "Paper", "Scissors"};
     private int cpuWins = 0;
     private int playerWins = 0;
+    Image image;
 
     @FXML
-    protected void scissorsButtonClicked() {
+    protected void scissorsButtonClicked() {  // WHY the buttons scale differently in scenebuilder? God knows.
+        playerChoiceText.setText("You chose: Scissors");
+        image = new Image(new File("src\\main\\images\\scissors.jpg").toURI().toString());
+        playerChoiceImage.setImage(image);
         playRound("Scissors");
     }
     @FXML
     protected void paperButtonClicked() {
+        playerChoiceText.setText("You chose: Paper");
+        image = new Image(new File("src\\main\\images\\paper.jpg").toURI().toString());
+        playerChoiceImage.setImage(image);
         playRound("Paper");
     }
     @FXML
     protected void rockButtonClicked() {
+        playerChoiceText.setText("You chose: Rock");
+        image = new Image(new File("src\\main\\images\\rock.jpg").toURI().toString());
+        playerChoiceImage.setImage(image);
         playRound("Rock");
     }
 
     private void playRound(String playerChoice) {
-        cpuText.setText("");
+        cpuChoiceText.setText("");
         playerWinText.setText("Player wins: " + playerWins);
         cpuWinText.setText("Cpu wins: " + cpuWins);
 
         int randomChoice = random.nextInt(3);
-        cpuText.setText("Cpu chose: " + cpuChoice[randomChoice]);
+        cpuChoiceText.setText("Cpu chose: " + cpuChoice[randomChoice]);
         String cpuMove = cpuChoice[randomChoice];
 
-        Image image = null;
+
         switch(cpuMove)
         {
             case "Rock":
@@ -62,14 +75,12 @@ public class HelloController {
         }
         cpuChoiceImage.setImage(image);
 
-
         if (cpuChoice[randomChoice].equalsIgnoreCase(playerChoice)) {
             outcomeText.setStyle("-fx-text-fill: black;");
             outcomeText.setText("It's a tie!");
         } else if ((cpuChoice[randomChoice].equalsIgnoreCase("Rock") && playerChoice.equalsIgnoreCase("Scissors")) ||
                 (cpuChoice[randomChoice].equalsIgnoreCase("Paper") && playerChoice.equalsIgnoreCase("Rock")) ||
                 (cpuChoice[randomChoice].equalsIgnoreCase("Scissors") && playerChoice.equalsIgnoreCase("Paper"))) {
-
             cpuWins++;
             outcomeText.setStyle("-fx-text-fill: red;");
             outcomeText.setText("You lose!");
